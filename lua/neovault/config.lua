1765dac9abbaf8a33bef49474a1594d840efd87f
River Chubb <Riversidechat@gmail.com>
2024-04-17 16:38:06 -0400

    feat: add config defaults

--- lua/neovault/config.lua
+++ lua/neovault/config.lua
@@ -8,16 +8,31 @@ local utils = require('neovault.utils')
 
 ---@class VaultConfig
 ---@field register_path? string | function
+---@field key? string | fun(): string
 ---@field encode? fun(obj: table): string
 ---@field decode? fun(str: string): table
 
+---Get the default Neovault config
+---@return Config
 M.defaults = function()
-    return {
+    ---@type Config
+    local defaults = {
         timeout = true,
         vault = {
             register_path = utils.join_paths(vim.fn.stdpath('state'), 'neovault'),
+            key = function()
+                return assert(vim.loop.cwd())
+            end,
+            encode = function(obj)
+                return assert(vim.json.encode(obj))
+            end,
+            decode = function(str)
+                return assert(vim.json.decode(str))
+            end,
         }
     }
+
+    return defaults
 end
 
 return M
